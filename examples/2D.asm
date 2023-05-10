05da69f5b74492666b041c04fc3b8b77f70dc763
Blitz3DFASM <133161792+Blitz3DFASM@users.noreply.github.com>
2023-05-10 23:35:30 +0300

    Add files via upload

---
+++ examples/2D.asm
@@ -0,0 +1,78 @@
+;******************************************************
+;*                  Blitz3D on FASM                   *
+;******************************************************
+
+format PE GUI 4.0
+entry start
+
+include 'win32a.inc'
+include 'blitz3dsdk.inc'
+
+; ------------ константы и переменные -----------------
+section '.data' data readable writeable
+
+camera BBCamera 0
+light BBLight 0
+cone BBModel 0
+font BBFont 0
+
+r dd 0
+g dd 0
+b dd 0
+
+font_name db 'System',0
+text db "Simple Text! ѕростой текст",0
+title db 'Blitz3D 2D Graphics Exmple on FASM', 0
+exit db 'Do you really want to exit?', 0
+empty_str db 0
+
+; ------------------------- код ------------------------
+section '.code' code readable executable
+
+start:
+      cinvoke bbBeginBlitz3D
+      cinvoke bbSetBlitz3DTitle, title, exit
+      cinvoke bbGraphics, 640, 480, 32, 3
+
+      cinvoke bbLoadFont, font_name, 32, BBTRUE, BBFALSE, BBTRUE
+      mov [font],eax
+      cinvoke bbSetFont, [font]
+
+main_2dloop:
+      cinvoke bbKeyHit, KEY_ESCAPE
+      or eax, eax
+      jnz exit_from_main_loop           ; If Esc pressed then exit from app
+
+      cinvoke bbClsColor, [r], [g], [b]  ; Set color and clear screen
+      cinvoke bbCls
+      inc [r]
+      add [g],7
+      add [b],3
+
+      cinvoke bbLine, 20, 20, 20, 460
+      cinvoke bbColor, 0, 128, 255
+      cinvoke bbRect, 40, 40, 50, 50, BBTRUE
+      cinvoke bbColor, 255, 255, 255
+      cinvoke bbText, 320, 220, text, BBTRUE, BBFALSE
+
+      cinvoke bbFlip                     ; Show buffer
+
+      jmp main_2dloop;
+
+exit_from_main_loop:
+      cinvoke bbEndBlitz3D
+      invoke ExitProcess,0
+
+section '.idata' import data readable writeable
+
+library kernel32,'KERNEL32.DLL',\
+       user32,'USER32.DLL',\
+       b3d,'B3D.DLL'
+
+       include 'api\kernel32.inc'
+       include 'api\user32.inc'
+       include 'b3d.inc'
+
+section '.rsrc' resource data readable
+       ICON equ '3D.ico'
+       include 'res.inc'
---
+++ examples/Print.asm
@@ -0,0 +1,108 @@
+;******************************************************
+;*                  Blitz3D on FASM                   *
+;*                                                    *
+;*     Example of using b3dmacro.inc TextXY macro     *
+;******************************************************
+
+format PE GUI 4.0
+entry start
+
+include 'win32a.inc'
+include 'blitz3dsdk.inc'
+
+; ------------ константы и переменные -----------------
+section '.data' data readable writeable
+
+include 'b3dmacro.inc'
+
+buffer BBCanvas 0
+camera BBCamera 0
+light BBLight 0
+
+cone BBModel 0
+cube BBModel 0
+sphere BBModel 0
+
+title db 'Blitz3D 3D Graphics Exmple on FASM', 0
+text db 'Class of cube is: ', 0
+exit db 'Do you really want to exit?', 0
+empty_str db 0
+class_name dd 0      ; Pointer to string
+
+string db 'This is string',0
+
+strFormat db 'EBP pointer is: %d ESP pointer is: %d',0
+strBuffer rb 256
+val dd 0
+
+; ------------------------- код ------------------------
+section '.code' code readable executable
+
+start:
+      cinvoke bbBeginBlitz3D
+      cinvoke bbSetBlitz3DTitle, title, empty_str
+      cinvoke bbGraphics3D, 640, 480, 32, 3
+
+      cinvoke bbBackBuffer
+      mov [buffer],eax
+      cinvoke bbSetBuffer, [buffer]
+
+      cinvoke bbCreateCamera, 0            ; paerent 0
+      mov [camera], eax
+      cinvoke bbCreateLight, 1, 0          ; 1 - direct light, parent null
+      mov [light], eax
+
+      cinvoke bbCreateCube, 0              ; parent null
+      mov [cube], eax
+      cinvoke bbPositionEntity, [cube], 0f, 0f, 4f
+      cinvoke bbEntityColor, [cube], 150f, 5f, 150f
+
+      cinvoke bbEntityClass, [cube]
+      mov [class_name], eax    ; Save pointer to string
+
+main_loop:
+      cinvoke bbKeyHit, KEY_ESCAPE ; Wait Esc
+      or eax, eax
+      jnz exit_from_main_loop
+
+      cinvoke bbTurnEntity, [cube], 0.5f, 2f, 1f
+
+      cinvoke bbRenderWorld, 1      ; Render world to buffer
+
+
+      TextXY 1, 1, 'Class of cube is: %s', [class_name]
+
+      cinvoke bbText,   1, 20, strBuffer, BBFALSE, BBFALSE
+
+
+      cinvoke bbEntityYaw, [cube]
+      fistp [val]  ; float to integer
+      TextXY 0, 40, 'Cube Yaw:   %i', [val]
+      cinvoke bbEntityPitch, [cube]
+      fistp [val]  ; float to integer
+      TextXY 0, 50, 'Cube Pitch: %i', [val]
+      cinvoke bbEntityRoll, [cube]
+      fistp [val]  ; float to integer
+      TextXY 0, 60, 'Cube Roll:  %i', [val]
+
+      TextXY 0, 100, 'Example of printg values from macro: %d %X %x %s', -24, 0xC0DE0ACE, 0x0DECADE, string
+
+      cinvoke bbFlip, BBTRUE       ; Flip buffers
+      jmp main_loop;
+exit_from_main_loop:
+      cinvoke bbEndBlitz3D
+      invoke ExitProcess,0
+
+section '.idata' import data readable writeable
+
+library kernel32,'KERNEL32.DLL',\
+       user32,'USER32.DLL',\
+       b3d,'B3D.DLL'
+
+       include 'api\kernel32.inc'
+       include 'api\user32.inc'
+       include 'b3d.inc'
+
+section '.rsrc' resource data readable
+       ICON equ '3D.ico'
+       include 'res.inc'
